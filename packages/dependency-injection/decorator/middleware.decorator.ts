93a6f1eee7d028df867fb1658bfcc801d796d29b
Roman Eckert <mail@romaneckert.de>
2020-06-15 23:17:28 +0200

    update middleware decorator

--- packages/dependency-injection/decorator/middleware.decorator.ts
+++ packages/dependency-injection/decorator/middleware.decorator.ts
@@ -1,7 +1,7 @@
 import { ContainerComponent } from '../component/container.component';
 
-export const MiddlewareDecorator = (): ClassDecorator => {
+export const MiddlewareDecorator = (options?: { before?: string; after?: string }): ClassDecorator => {
     return (target): void => {
-        ContainerComponent.register(target);
+        ContainerComponent.register(target, options);
     };
 };
