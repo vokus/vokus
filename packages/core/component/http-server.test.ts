1e5f2ea1cd522f83b86da4ba9403e18c187f55d6
Roman Eckert <mail@romaneckert.de>
2020-06-29 00:38:27 +0200

    add some http tests

--- packages/core/component/http-server.test.ts
+++ packages/core/component/http-server.test.ts
@@ -13,36 +13,58 @@ afterAll(async () => {
     await FileSystem.remove(Environment.configPath);
 });
 
-test('http-server', async () => {
-    const httpServer: HttpServer = await ObjectManager.get(HttpServer);
+describe('http-server', () => {
+    test('config', async () => {
+        const httpServer: HttpServer = await ObjectManager.get(HttpServer);
 
-    expect(httpServer.listening).toBe(false);
+        expect(httpServer.config).toStrictEqual({ port: 443 });
+    });
 
-    await httpServer.start();
+    test('addConfig', async () => {
+        const httpServer: HttpServer = await ObjectManager.get(HttpServer);
 
-    expect(httpServer.listening).toBe(true);
+        httpServer.addConfig({ port: 3000 });
 
-    const httpClient: HttpClient = await ObjectManager.get(HttpClient);
+        expect(httpServer.config).toStrictEqual({ port: 3000 });
+    });
 
-    expect((await httpClient.get('https://localhost:3000/')).statusCode).toBe(404);
-    expect(httpServer.selfSigned).toBe(true);
+    test('listening', async () => {
+        const httpServer: HttpServer = await ObjectManager.get(HttpServer);
 
-    await httpServer.stop();
+        expect(httpServer.listening).toBe(false);
 
-    expect(httpServer.listening).toBe(false);
+        await httpServer.start();
 
-    await FileSystem.copyFile(
-        path.join(__dirname, '../self-signed-key.pem'),
-        path.join(Environment.configPath, 'http-server', 'key.pem'),
-    );
-    await FileSystem.copyFile(
-        path.join(__dirname, '../self-signed-cert.pem'),
-        path.join(Environment.configPath, 'http-server', 'cert.pem'),
-    );
+        expect(httpServer.listening).toBe(true);
 
-    await httpServer.start();
+        await httpServer.stop();
+    });
 
-    expect((await httpClient.get('https://localhost:3000/')).statusCode).toBe(404);
+    test('http-server', async () => {
+        const httpServer: HttpServer = await ObjectManager.get(HttpServer);
 
-    await httpServer.stop();
+        await httpServer.start();
+
+        const httpClient: HttpClient = await ObjectManager.get(HttpClient);
+
+        expect((await httpClient.get('https://localhost:3000/')).statusCode).toBe(404);
+        expect(httpServer.selfSigned).toBe(true);
+
+        await httpServer.stop();
+
+        await FileSystem.copyFile(
+            path.join(__dirname, '../self-signed-key.pem'),
+            path.join(Environment.configPath, 'http-server', 'key.pem'),
+        );
+        await FileSystem.copyFile(
+            path.join(__dirname, '../self-signed-cert.pem'),
+            path.join(Environment.configPath, 'http-server', 'cert.pem'),
+        );
+
+        await httpServer.start();
+
+        expect((await httpClient.get('https://localhost:3000/')).statusCode).toBe(404);
+
+        await httpServer.stop();
+    });
 });
