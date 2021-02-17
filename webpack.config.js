0742cd03cd40c451ebe2732ea5c8f6bb0fef9afb
Roman Eckert <mail@romaneckert.de>
2021-02-17 11:53:32 +0100

    work on webpack integration

--- webpack.config.js
+++ webpack.config.js
@@ -1,55 +1,34 @@
-const autoprefixer = require('autoprefixer');
+const path = require('path');
 
 module.exports = {
-    // entry: ['.packages/ui/scss/site.scss', './app.js'],
-    entry: ['./packages/app/scss/app.scss'],
+    entry: ['./packages/app/js/app.ts'],
     mode: 'production',
     module: {
         rules: [
             {
                 test: /\.scss$/,
                 use: [
-                    {
-                        loader: 'file-loader',
-                        options: {
-                            name: './packages/app/public/assets/app/css/app.css',
-                            path: __dirname,
-                        },
-                    },
-                    { loader: 'extract-loader' },
-                    { loader: 'css-loader' },
-                    /* TODO: fix
-                    {
-                        loader: 'postcss-loader',
-                        options: {
-                            plugins: () => [autoprefixer()],
-                        },
-                    },*/
-                    {
-                        loader: 'sass-loader',
-                        options: {
-                            implementation: require('sass'),
-                            sassOptions: {
-                                includePaths: ['./node_modules'],
-                            },
-
-                            // see https://github.com/webpack-contrib/sass-loader/issues/804
-                            webpackImporter: false,
-                        },
-                    },
+                    "style-loader",
+                    "css-loader",
+                    "sass-loader",
                 ],
             },
             {
-                loader: 'babel-loader',
-                options: {
-                    presets: ['@babel/preset-env'],
+                test: /\.woff2$/,
+                use: {
+                    loader: 'file-loader',
+                    options: {
+                        include: path.resolve(__dirname, './packages/app/font/'),
+                        name: '[name].[ext]',
+                        outputPath: './font/'
+                    },
                 },
-                test: /\.js$/,
             },
         ],
     },
     output: {
-        filename: './packages/app/public/assets/app/js/app.js',
-        path: __dirname,
+        filename: './js/app.js',
+        path: path.resolve(__dirname, 'packages/app/public/assets/app'),
     },
 };
+
